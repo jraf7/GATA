3058ff58a6a9b8c6a1f8a70d4edef1858f3b1388
Devante <devante.jones229@gmail.com>
2020-01-04 18:25:10 -0500

    adding initial structure to front-end

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+import MenuAppBar from "./components/MenuBar";
+import UserContainer from "./components/UserContainer";
+import TrackButton from "./components/TrackButton";
+import StatsButton from "./components/StatsButtton";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { red, grey } from "@material-ui/core/colors";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 
-function App() {
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: red[400],
+      main: red[600],
+      dark: red[900],
+      contrastText: "#000"
+    },
+    secondary: {
+      light: grey[400],
+      main: grey[600],
+      dark: grey[900],
+      contrastText: "#000"
+    }
+  }
+});
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    textAlign: "center"
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary
+  }
+}));
+
+export default function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <MenuAppBar />
+          </Grid>
+          <Grid item xs={12}>
+            <UserContainer />
+          </Grid>
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+            item
+            xs={6}
+          >
+            <StatsButton />
+          </Grid>
+          <Grid item xs={6}>
+            <TrackButton />
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>xs=3</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>xs=3</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>xs=3</Paper>
+          </Grid>
+        </Grid>
+      </div>
+    </ThemeProvider>
   );
 }
-
-export default App;
