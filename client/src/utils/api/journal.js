bffc612de4aab1d110e4439cd9526b2512222295
Jonathan Johnson <jraf7@icloud.com>
2020-01-08 20:58:11 -0500

    syntax apis

--- client/src/utils/api/journal.js
+++ client/src/utils/api/journal.js
@@ -2,19 +2,19 @@ import axios from "axios";
 
 const journal =  {
     get: function () {
-        return axios.get("/api/user/journal");
+        return axios.get("/api/user/journals");
     },
     //get task
     getOne: function (id) {
-        return axios.get("/api/user/journal" + id);
+        return axios.get("/api/user/journals" + id);
     },
     //delete tasks
     delete: function(id) {
-        return axios.delete("/api/user/journal/" + id);
+        return axios.delete("/api/user/journals/" + id);
     },
     //save a task
     save: function (taskData) {
-        return axios.post("/api/user/journal", taskData);
+        return axios.post("/api/user/journals", taskData);
     }
 }
 
